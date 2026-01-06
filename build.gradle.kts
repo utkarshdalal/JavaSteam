1b81cf565b8a8843db29019aaffa47d0a531b2e0
Joshua Tam <297250+joshuatam@users.noreply.github.com>
2026-01-06 10:49:00 +0800

    Adds option to disable automatic download start
    Introduces a new `autoStartDownload` parameter to the `DepotDownloader` class.
    
    This allows users to initialize the downloader without immediately starting the download process, providing more control over when the download begins.
    
    The version is incremented to indicate a new snapshot release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
 
 allprojects {
     group = "io.github.joshuatam"
-    version = "1.8.0-2-SNAPSHOT"
+    version = "1.8.0-3-SNAPSHOT"
 }
 
 repositories {
--- javasteam-depotdownloader/src/main/kotlin/in/dragonbra/javasteam/depotdownloader/DepotDownloader.kt
+++ javasteam-depotdownloader/src/main/kotlin/in/dragonbra/javasteam/depotdownloader/DepotDownloader.kt
@@ -105,6 +105,7 @@ import kotlin.text.toLongOrNull
  * @param maxFileWrites Number of concurrent files being written. Default: 1
  * @param androidEmulation Forces "Windows" as the default OS filter. Used when running Android games in PC emulators that expect Windows builds.
  * @param parentJob Parent job for the downloader. If provided, the downloader will be cancelled when the parent job is cancelled.
+ * @param autoStartDownload Whether to start downloading automatically. If false, you must call [startDownloading] manually.
  *
  * @author Oxters
  * @author Lossy
@@ -122,6 +123,7 @@ class DepotDownloader @JvmOverloads constructor(
     private var maxFileWrites: Int = 1,
     private val androidEmulation: Boolean = false,
     private val parentJob: Job? = null,
+    private val autoStartDownload: Boolean = true,
 ) : Closeable {
 
     companion object {
@@ -243,6 +245,12 @@ class DepotDownloader @JvmOverloads constructor(
             }
         }
 
+        if (autoStartDownload) {
+            startDownloading()
+        }
+    }
+
+    fun startDownloading() {
         // Launch the processing loop
         scope.launch {
             processItems()
