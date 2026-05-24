2a2e45283f5e40771ff6f7a670d12a3194476c83
Phobos665 <5970062+phobos665@users.noreply.github.com>
2026-05-24 15:09:40 +0100

    fix(): Fixed timestamp for expanded achievements (#12)
    * fix(): Fixed timestamp for expanded achievements
    
    * fix(): revert the param optionality.

--- src/main/java/in/dragonbra/javasteam/steam/handlers/steamuserstats/callback/UserStatsCallback.kt
+++ src/main/java/in/dragonbra/javasteam/steam/handlers/steamuserstats/callback/UserStatsCallback.kt
@@ -123,7 +123,8 @@ class UserStatsCallback(packetMsg: IPacketMsg?) : CallbackMsg() {
                 if (bitsBlock != KeyValue.INVALID) {
                     // This block has bit-level achievements, expand them and get the values
                     for (bitEntry in bitsBlock.children) {
-                        val bitIndex = bitEntry["bit"].asInteger()
+                        // Grabbing bitIndex for Timestamp parsing.
+                        val bitIndex = bitEntry.name?.toIntOrNull() ?: 0
                         val displaySection = bitEntry["display"]
 
                         // Extract metadata
